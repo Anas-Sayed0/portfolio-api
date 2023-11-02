023b364302511c863ece4bbdba259f6c7878bded
Anas-Sayed0 <anas11turbo@gmail.com>
2023-11-02 20:18:52 +0200

    blabla

--- server.js
+++ server.js
@@ -29,29 +29,26 @@ contactEmail.verify((error) => {
   }
 });
 
-router.get("/", (req, res) => {
-  // const name = req.body.firstName + req.body.lastName;
-  // const email = req.body.email;
-  // const message = req.body.message;
-  // const phone = req.body.phone;
-  // const mail = {
-  //   from: name,
-  //   to: "anas11turboxxx@gmail.com",
-  //   subject: "Contact Form Submission - Portfolio",
-  //   html: `<p>Name: ${name}</p>
-  //          <p>Email: ${email}</p>
-  //          <p>Phone: ${phone}</p>
-  //          <p>Message: ${message}</p>`,
-  // };
-  // contactEmail.sendMail(mail, (error) => {
-  //   if (error) {
-  //     res.json(error);
-  //   } else {
-  //     res.json({ code: 200, status: "Message Sent" });
-  //   }
-  // });
-  res.json({ status: "Message Sent" });
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "anas11turboxxx@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
 });
-
-
 module.exports = app;
